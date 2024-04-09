d75fe00a0b6f1f4d4eceb37b7efc02937a120863
Koushik S <koushik_2031@yahoo.com>
2024-04-09 16:49:29 +0530

    Update controlnet.py

--- controlnet.py
+++ controlnet.py
@@ -21,7 +21,7 @@ pipe = StableDiffusionXLControlNetPipeline.from_pretrained(model_id_or_path, con
 pipe.enable_model_cpu_offload()
 # image = load_image("https://huggingface.co/lllyasviel/sd-controlnet-hed/resolve/main/images/bird.png")
 
-init_image = Image.open("assets/myface/Atenea-K19-05-b.png").convert("RGB")
+init_image = Image.open("assets/myface/Koushik-K19-05-b.png").convert("RGB")
 width, height = init_image.size
 ratio = width / height
 new_height = 800
@@ -46,11 +46,8 @@ canny_image.save(f"output/init_images_canny.png")
 # pipe.enable_xformers_memory_efficient_attention()
 # pipe.enable_model_cpu_offload()
 
-prompt = "a women sculpture with fully exposed shoulder chest and boobs, broad shoulder, midriff, waist, extremely detailed, photo-realistic, high quality, (extremely detailed eyes face and hands)"
-# prompt = "Sexy Pallas Athena standing on a globe, holding a spear in her left hand and her shield in her right hand, Roman Soldier Helmet, Nude, boobs, same pose, extremely detailed, photo-realistic, high quality, (extremely detailed eyes face and hands)"
+prompt = "Egyptian pharaoh, extremely detailed, photo-realistic, high quality, (extremely detailed eyes face and hands)"
 neg_prompt  = "ugly, facial hair, body hair, deformed, disfigured, poor details, bad anatomy, lowres, mutant, cropped, worst quality, low quality, jpeg artifacts, signature, watermark, username, blurry, made by children, caricature, ugly, boring, sketch, lacklustre, repetitive, cropped, (long neck), body horror, out of frame, mutilated, tiled, frame, border, porcelain skin, doll"
-# prompt = "hindu goddess parvati standing view from backside show the entire toned Booty and cracks, Female Booty Shower, hip, booty, full body shot, best quality, high quality"
-# all_images = pipe(prompt=prompt, strength=0.75, guidance_scale=12).images
 all_images = pipe(prompt=prompt, negative_prompt=neg_prompt, controlnet_conditioning_scale=controlnet_conditioning_scale, image=canny_image, num_inference_steps=20, strength=0.8, guidance_scale=12).images
 
 n = 0
