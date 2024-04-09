122ab1f9520c7cec326828162d27a18b57c3d8cc
Koushik S <koushik_2031@yahoo.com>
2024-04-09 16:35:37 +0530

    Update stable_canny.py

--- stable_canny.py
+++ stable_canny.py
@@ -71,7 +71,6 @@ def make_canny_condition(image):
 
 
 # get canny image
-# init_image = Image.open("assets/myface/booty_1.jpg").convert("RGB")
 init_image = Image.open("assets/images/athena_merge.jpg").convert("RGB")
 # mask_image = Image.open("assets/bust/athena_bust_mask.jpg").convert("RGB")
 # control_image = make_inpaint_condition(init_image, mask_image)
@@ -90,19 +89,8 @@ width, height = init_image.size
 # low_threshold = 100
 # high_threshold = 200
 
-# prompt = "A nude italian women, (perky_breasts:1.1), <lora:MyBreastHelper15RS:0.4>, BREAK photo realistic, Ultra realistic, high quality, extremely detailed."
-# prompt = "Nude, Small breasts. Fantasy, Intricate, Detailed, colourfull, Constrain Proportions, Ultra realistic. Photography. Portrait."
-# prompt = "a naked french woman, big breasts!, erect nipples, holding helmet, beautiful symmetric body, muscles, fit, athletic. smoth edjes, (high detailed skin:1.2). Ultra realistic. Photography. professional, 4k, highly detailed, photo realistic, high quality."
-# prompt = "marble sculpture bust, Nude, bell shape boobs amd nipples, deep cleavage, (nude upper body:1.9), photo realistic, Ultra realistic, high quality, extremely detailed."
-# prompt = "marble sculpture of naked warrior women, smoth edjes, hair bun, full boobs and hard nipples, arms facing down, bare shoulder, no belly bottom, full boobs over statue holder, (bare shoulder:1.9), (bare chest:1.9), extremely detailed chest, photo-realistic, high quality"
-# prompt = "copper statue of naked women, full (boobs:1.9) erect (nipples:1.9), smoth edjes, bare neck shoulder and arms, navel, (waist:1.9), (navel:1.9), (bare shoulder:1.9), (bare chest:1.9), photo-realistic, high quality, (extremely detailed chest and belly)"
-# prompt = "marble bust, white flawless, firm round raised boobs and nipples, (upper body:1.5), bare masculine shoulder and arms, hair bun, hyper details, «bare shoulders», extremely detailed, photo-realistic, high quality, all on Stable Diffusion 1.5 base model."
-prompt = "nude couple, nude lovers, greek women with greek warrior helmet with a young man, goddess, Sexy and kinky, hyper realistic, high quality"
-# prompt = "a naked women showing booty, photo realistic, Ultra realistic, high quality, extremely detailed. all on Stable Diffusion 1.5 base model."
-# prompt = "Sexy Pallas Athena standing on a globe, holding a spear in her left hand and her shield in her right hand, Roman Soldier Helmet, Nude, boobs, same pose, extremely detailed, photo-realistic, high quality, (extremely detailed eyes face and hands)"
+prompt = "Athena standing on a globe, holding a spear in her left hand and her shield in her right hand, Roman Soldier Helmet"
 neg_prompt = "ugly, deformed, disfigured, poor details, bad anatomy, free hair, mutant, cropped, worst quality, low quality, jpeg artifacts, signature, watermark, username, blurry, made by children, caricature, ugly, boring, sketch, lacklustre, repetitive, cropped, (long neck), body horror, out of frame, mutilated, tiled, frame, border, porcelain skin"
-# # prompt = "hindu goddess parvati standing view from backside show the entire toned Booty and cracks, Female Booty Shower, hip, booty, full body shot, best quality, high quality"
-# # all_images = pipe(prompt=prompt, strength=0.75, guidance_scale=12).images
 
 n = 0
 for item in tqdm(combined_list, total=len(combined_list)):
